3aeaec3e7cdcf5d3de33a8522f13435232d4964a
voddan <dgvodopyan@gmail.com>
2018-09-06 13:16:22 +0300

    Extracted `poolConfig` parameter from `PoolManager.openIndyPool`; Did minor refactor

--- indy-utils/src/main/kotlin/com/luxoft/blockchainlab/hyperledger/indy/utils/PoolManager.kt
+++ indy-utils/src/main/kotlin/com/luxoft/blockchainlab/hyperledger/indy/utils/PoolManager.kt
@@ -2,6 +2,7 @@ package com.luxoft.blockchainlab.hyperledger.indy.utils
 
 import org.hyperledger.indy.sdk.pool.Pool
 import org.hyperledger.indy.sdk.pool.PoolJSONParameters
+import org.hyperledger.indy.sdk.pool.PoolJSONParameters.OpenPoolLedgerJSONParameter
 import org.hyperledger.indy.sdk.pool.PoolLedgerConfigExistsException
 import java.io.File
 import java.util.concurrent.ConcurrentHashMap
@@ -17,30 +18,25 @@ object PoolManager {
 
     private val openIndyPools = ConcurrentHashMap<String, Pool>()
 
-    fun openIndyPool(genesisFile: File, poolName: String = DEFAULT_POOL_NAME): Pool {
+    fun openIndyPool(genesisFile: File,
+                     poolName: String = DEFAULT_POOL_NAME,
+                     poolConfig: OpenPoolLedgerJSONParameter = OpenPoolLedgerJSONParameter(null, null, null)
+    ): Pool {
         val pool = openIndyPools.getOrPut(poolName) {
-            val configName = createPoolLedgerConfigFromFile(genesisFile, poolName)
+            val ledgerConfig = PoolJSONParameters.CreatePoolLedgerConfigJSONParameter(genesisFile.absolutePath)
+
+            try {
+                Pool.createPoolLedgerConfig(poolName, ledgerConfig.toJson()).get()
+            } catch (e: ExecutionException) {
+                if(e.cause !is PoolLedgerConfigExistsException) throw e
+                // ok
+            }
 
             Pool.setProtocolVersion(2).get()
 
-            val config = PoolJSONParameters.OpenPoolLedgerJSONParameter(null, null, null)
-
-            Pool.openPoolLedger(configName, config.toJson()).get()
+            Pool.openPoolLedger(poolName, poolConfig.toJson()).get()
         }
 
         return pool
     }
-}
-
-fun createPoolLedgerConfigFromFile(genesisFile: File, poolName: String = PoolManager.DEFAULT_POOL_NAME): String {
-    val ledgerConfig = PoolJSONParameters.CreatePoolLedgerConfigJSONParameter(genesisFile.absolutePath)
-
-    try {
-        Pool.createPoolLedgerConfig(poolName, ledgerConfig.toJson()).get()
-    } catch (e: ExecutionException) {
-        if(e.cause !is PoolLedgerConfigExistsException) throw e
-        // ok
-    }
-
-    return poolName
 }
